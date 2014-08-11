f2a1fe1779d70987d653657bd7f55f55d5d10357
SirGFM <gfmandaji@hotmail.com>
2014-08-11 19:03:29 +0000

    Added tilemap source and header, but still haven't implemented anything

---
+++ include/GFraMe/GFraMe_tilemap.h
@@ -0,0 +1,16 @@
+/**
+ * @include/GFraMe/GFraMe_tilemap.h
+ */
+#ifndef __GFRAME_TILEMAP_H
+#define __GFRAME_TILEMAP_H
+
+#include <GFraMe/GFraMe_spriteset.h>
+#include <GFraMe/GFraMe_texture.h>
+
+struct stGFraMe_tilemap {
+	
+};
+typedef struct stGFraMe_tilemap GFraMe_tilemap;
+
+#endif
+
---
+++ src/gframe_tilemap.c
@@ -0,0 +1,8 @@
+/**
+ * @src/gframe_tilemap.c
+ */
+#include <GFraMe/error.h>
+#include <GFraMe/GFraMe_spriteset.h>
+#include <GFraMe/GFraMe_texture.h>
+#include <GFraMe/GFraMe_tilemap.h>
+
