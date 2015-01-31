b403908909ce1a64a56985d76761118b7a3b078e
SirGFM <gfmandaji@hotmail.com>
2015-01-30 23:40:30 -0200

    Test commented

--- tst/gframe_test_collision.c
+++ tst/gframe_test_collision.c
@@ -12,21 +12,34 @@
 #include <GFraMe/GFraMe_spriteset.h>
 #include <GFraMe/GFraMe_texture.h>
 
-static GFraMe_sprite s1, s2;
-static GFraMe_spriteset sset;
-static GFraMe_texture atlas;
-static int dstX, dstY, didInitAssets, running, test;
-
-// Define some variables needed by the events module
-GFraMe_event_setup();
-
+/**
+ * Window's width
+ */
 #define WND_W 320
+/**
+ * Window's height
+ */
 #define WND_H 240
+/**
+ * Sprite's (default) width
+ */
 #define SPR_H 8
+/**
+ * Sprite's (default) height
+ */
 #define SPR_W 8
+/**
+ * Sprite (tiles) count
+ */
 #define SPR_C 2
-#define BPC 4 // Bytes Per Color
+/**
+ * Bytes per color (packet in RGBA format)
+ */
+#define BPC 4
 
+/**
+ * Anonymous enumeration for tests
+ */
 enum {
     TEST_INI        = 0,
     TEST_LEFT       = 0,
@@ -40,6 +53,9 @@ enum {
     TEST_END        = 8
 };
 
+/**
+ * Store sprite1's initial state
+ */
 struct test_params {
     int x;
     int y;
@@ -52,17 +68,219 @@ struct test_params {
     char *label;
 };
 
+/**
+ * Sprites for testing collision
+ */
+static GFraMe_sprite s1, s2;
+/**
+ * Spriteset used by the sprites
+ */
+static GFraMe_spriteset sset;
+/**
+ * Texture used by the sprites
+ */
+static GFraMe_texture atlas;
+/**
+ * Whether the assets where initialized
+ */
+static int didInitAssets;
+/**
+ * Keep the main loop running
+ */
+static int running;
+/**
+ * Keep track of the current test
+ */
+static int test;
+/**
+ * Store every possible test configuration
+ */
+struct test_params tests[TEST_END];
+
+// Define some variables needed by the events module
+GFraMe_event_setup();
+
+/**
+ * Initialize the assets
+ * 
+ * return GFraMe error code
+ */
 static GFraMe_ret init_assets();
+/**
+ * Clean up assets
+ */
 static void clean_assets();
-
+/**
+ * Check which event ocurred (necessary for stepping the timer and quitting)
+ */
 static void event_handler();
+/**
+ * Initialize the tests parameters
+ */
+static void init_tests();
 
+/**
+ * Main function.
+ * 
+ * @param argc Number of arguments
+ * @param argv The actual arguments
+ * @return Error code
+ */
 int main (int argc, char *argv[]) {
     GFraMe_ret rv;
-    int i;
-    struct test_params tests[TEST_END];
     
+    // Mark assets as not needing clean up
     didInitAssets = 0;
+    
+    // Init the framework
+    rv = GFraMe_init(WND_W, WND_H, WND_W, WND_H, "com.gfmgamecorner",
+        "CollisionTest", GFraMe_window_resizable, 0, 60, 0, 0);
+    GFraMe_assertRet(rv == GFraMe_ret_ok, "Failed to init the framework", __ret);
+    
+    // Init the assets
+    rv = init_assets();
+    GFraMe_assertRet(rv == GFraMe_ret_ok, "Failed to init assets", __ret);
+    
+    // Initialize the tests
+    init_tests();
+    
+    // Init the framework update and draw clock
+    GFraMe_event_init(60, 60);
+    
+    // Run every test
+    running = 1;
+    test = TEST_INI;
+    while (test < TEST_END) {
+        struct test_params *t;
+        
+        // Get the current test configuration and the both players' params
+        t = &tests[test];
+        
+        GFraMe_sprite_init(&s1, t->x, t->y, t->w, t->h, &sset, 0, 0);
+        s1.cur_tile = 0;
+        GFraMe_sprite_get_object(&s1)->vx = t->vx;
+        GFraMe_sprite_get_object(&s1)->vy = t->vy;
+        
+        GFraMe_sprite_init(&s2, (WND_W - SPR_W) / 2, (WND_H - SPR_H) / 2, SPR_W,
+            SPR_H, &sset, 0, 0);
+        s2.cur_tile = 1;
+        
+        // Run the main loop
+        running = 1;
+        while (running) {
+            event_handler();
+            GFraMe_event_update_begin();
+                // Update both sprites
+                GFraMe_sprite_update(&s1, GFraMe_event_elapsed);
+                GFraMe_sprite_update(&s2, GFraMe_event_elapsed);
+                
+                // Collide s1 with s2, but only move (i.e., separate) s1, on
+                // collision
+                rv = GFraMe_object_overlap(GFraMe_sprite_get_object(&s1),
+                    GFraMe_sprite_get_object(&s2), GFraMe_second_fixed);
+                
+                // If they collided, check the result and go to the next one
+                if (rv == GFraMe_ret_ok) {
+                    int resX, resY;
+                    
+                    resX = GFraMe_sprite_get_object(&s1)->x;
+                    resY = GFraMe_sprite_get_object(&s1)->y;
+                    
+                    GFraMe_log("%s", t->label);
+                    if (resX != t->dx || resY != t->dy)
+                        GFraMe_log("     Test failed!");
+                    else
+                        GFraMe_log("   Test succedded!");
+                    GFraMe_log("----------------------- ---");
+                    running = 0;
+                }
+            GFraMe_event_update_end();
+            GFraMe_event_draw_begin();
+                // Draw the sprites
+                GFraMe_sprite_draw(&s1);
+                GFraMe_sprite_draw(&s2);
+            GFraMe_event_draw_end();
+        }
+        
+        test++;
+    }
+    
+__ret:
+    
+    // Clean up everything
+    clean_assets();
+    GFraMe_quit();
+    return rv;
+}
+
+/**
+ * Initialize the assets
+ * 
+ * return GFraMe error code
+ */
+static GFraMe_ret init_assets() {
+    GFraMe_ret rv;
+    int i;
+    unsigned char pixels[SPR_W * SPR_H * SPR_C * BPC];
+    
+    // Initialize the pixels buffer
+    i = 0;
+    while (i < SPR_W * SPR_H * SPR_C * BPC) {
+        // Create a red tile ...
+        if (i % (SPR_W * SPR_C * BPC) < SPR_W * BPC) {
+            pixels[i + 0] = 0xff; // R
+            pixels[i + 1] = 0x00; // G
+            pixels[i + 2] = 0x00; // B
+            pixels[i + 3] = 0xff; // A
+        }
+        // ... and a blue one
+        else {
+            pixels[i + 0] = 0x00; // R
+            pixels[i + 1] = 0x00; // G
+            pixels[i + 2] = 0xff; // B
+            pixels[i + 3] = 0xff; // A
+        }
+        i += BPC;
+    }
+    
+    // Load the texture
+    rv = GFraMe_texture_load(&atlas, SPR_W * SPR_C, SPR_H, pixels);
+    GFraMe_assertRet(rv == GFraMe_ret_ok, "Failed to init the framework", __ret);
+    GFraMe_spriteset_init(&sset, &atlas, SPR_W, SPR_H);
+    
+    didInitAssets = 1;
+    rv = GFraMe_ret_ok;
+__ret:
+    return rv;
+}
+
+/**
+ * Clean up assets
+ */
+static void clean_assets() {
+    if (!didInitAssets)
+        return;
+    GFraMe_texture_clear(&atlas);
+}
+
+/**
+ * Check which event ocurred (necessary for stepping the timer and quitting)
+ */
+static void event_handler() {
+    GFraMe_event_begin();
+        GFraMe_event_on_timer();
+        GFraMe_event_on_quit();
+            GFraMe_log("Received quit!");
+            running = 0;
+            test = TEST_END;
+    GFraMe_event_end();
+}
+
+/**
+ * Initialize the tests parameters
+ */
+static void init_tests() {
+    int i;
     
     i = TEST_INI;
     while (i < TEST_END) {
@@ -173,124 +391,5 @@ int main (int argc, char *argv[]) {
         }
         i++;
     }
-    
-    // Init the framework
-    rv = GFraMe_init(WND_W, WND_H, WND_W, WND_H, "com.gfmgamecorner",
-        "CollisionTest", GFraMe_window_resizable, 0, 60, 0, 0);
-    GFraMe_assertRet(rv == GFraMe_ret_ok, "Failed to init the framework", __ret);
-    
-    rv = init_assets();
-    GFraMe_assertRet(rv == GFraMe_ret_ok, "Failed to init assets", __ret);
-    
-    GFraMe_event_init(60, 60);
-    
-    running = 1;
-    test = TEST_INI;
-    while (test < TEST_END) {
-        struct test_params *t;
-        
-        t = &tests[test];
-        
-        GFraMe_sprite_init(&s1, t->x, t->y, t->w, t->h, &sset, 0, 0);
-        s1.cur_tile = 0;
-        dstX = t->dx;
-        dstY = t->dy;
-        GFraMe_sprite_get_object(&s1)->vx = t->vx;
-        GFraMe_sprite_get_object(&s1)->vy = t->vy;
-        
-        GFraMe_sprite_init(&s2, (WND_W - SPR_W) / 2, (WND_H - SPR_H) / 2, SPR_W,
-            SPR_H, &sset, 0, 0);
-        s2.cur_tile = 1;
-        
-        // Run the main loop
-        running = 1;
-        while (running) {
-            event_handler();
-            GFraMe_event_update_begin();
-                GFraMe_sprite_update(&s1, GFraMe_event_elapsed);
-                GFraMe_sprite_update(&s2, GFraMe_event_elapsed);
-                
-                rv = GFraMe_object_overlap(GFraMe_sprite_get_object(&s1),
-                    GFraMe_sprite_get_object(&s2), GFraMe_second_fixed);
-                
-                if (rv == GFraMe_ret_ok) {
-                    int resX, resY;
-                    
-                    resX = GFraMe_sprite_get_object(&s1)->x;
-                    resY = GFraMe_sprite_get_object(&s1)->y;
-                    
-                    GFraMe_log("%s", t->label);
-                    if (resX != dstX || resY != dstY)
-                        GFraMe_log("     Test failed!");
-                    else
-                        GFraMe_log("   Test succedded!");
-                    GFraMe_log("----------------------- ---");
-                    running = 0;
-                }
-            GFraMe_event_update_end();
-            GFraMe_event_draw_begin();
-                GFraMe_sprite_draw(&s1);
-                GFraMe_sprite_draw(&s2);
-            GFraMe_event_draw_end();
-        }
-        
-        test++;
-    }
-    
-__ret:
-    
-    clean_assets();
-    GFraMe_quit();
-    return rv;
-}
-
-static GFraMe_ret init_assets() {
-    GFraMe_ret rv;
-    int i;
-    unsigned char pixels[SPR_W * SPR_H * SPR_C * BPC];
-    
-    // Initialize the pixels buffer
-    i = 0;
-    while (i < SPR_W * SPR_H * SPR_C * BPC) {
-        if (i % (SPR_W * SPR_C * BPC) < SPR_W * BPC) {
-            pixels[i + 0] = 0xff; // R
-            pixels[i + 1] = 0x00; // G
-            pixels[i + 2] = 0x00; // B
-            pixels[i + 3] = 0xff; // A
-        }
-        else {
-            pixels[i + 0] = 0x00; // R
-            pixels[i + 1] = 0x00; // G
-            pixels[i + 2] = 0xff; // B
-            pixels[i + 3] = 0xff; // A
-        }
-        i += BPC;
-    }
-    
-    // Load the texture
-    rv = GFraMe_texture_load(&atlas, SPR_W * SPR_C, SPR_H, pixels);
-    GFraMe_assertRet(rv == GFraMe_ret_ok, "Failed to init the framework", __ret);
-    GFraMe_spriteset_init(&sset, &atlas, SPR_W, SPR_H);
-    
-    didInitAssets = 1;
-    rv = GFraMe_ret_ok;
-__ret:
-    return rv;
-}
-
-static void clean_assets() {
-    if (!didInitAssets)
-        return;
-    GFraMe_texture_clear(&atlas);
-}
-
-static void event_handler() {
-    GFraMe_event_begin();
-        GFraMe_event_on_timer();
-        GFraMe_event_on_quit();
-            GFraMe_log("Received quit!");
-            running = 0;
-            test = TEST_END;
-    GFraMe_event_end();
 }
 
