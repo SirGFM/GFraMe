3b136110adcd6e303843b0626a95aef0ce5ad26b
SirGFM <gfmandaji@hotmail.com>
2016-02-03 21:56:13 -0200

    Added quadtree's color to gfmTypes.h

--- include/GFraMe/gfmTypes.h
+++ include/GFraMe/gfmTypes.h
@@ -7,38 +7,38 @@
 #define __GFMTYPES_H__
 
 // As much as I would like to use an enum, it wouldn't allow for user types...
-#define gfmType_none         0
-#define gfmType_object       1
-#define gfmType_sprite       2
-#define gfmType_tilemap      3
-#define gfmType_group        4
-#define gfmType_reserved_2   5
-#define gfmType_reserved_3   6
-#define gfmType_reserved_4   7
-#define gfmType_reserved_5   8
-#define gfmType_reserved_6   9
-#define gfmType_reserved_7   10
-#define gfmType_reserved_8   11
-#define gfmType_reserved_9   12
-#define gfmType_reserved_10  13
-#define gfmType_reserved_11  14
-#define gfmType_reserved_12  15
-#define gfmType_reserved_13  16
-#define gfmType_reserved_14  17
-#define gfmType_reserved_15  18
-#define gfmType_reserved_16  19
-#define gfmType_reserved_17  20
-#define gfmType_reserved_18  21
-#define gfmType_reserved_19  22
-#define gfmType_reserved_20  23
-#define gfmType_reserved_21  24
-#define gfmType_reserved_22  25
-#define gfmType_reserved_23  26
-#define gfmType_reserved_24  27
-#define gfmType_reserved_25  28
-#define gfmType_reserved_26  29
-#define gfmType_reserved_27  30
-#define gfmType_reserved_28  31
+#define gfmType_none         0  /* (light green) */
+#define gfmType_object       1  /* (red) */
+#define gfmType_sprite       2  /* (lightest blue) */
+#define gfmType_tilemap      3  /* (isn't actually drawn) */
+#define gfmType_group        4  /* (isn't actually drawn) */
+#define gfmType_reserved_2   5  /* (pink) */
+#define gfmType_reserved_3   6  /* (light blue) */
+#define gfmType_reserved_4   7  /* (green) */
+#define gfmType_reserved_5   8  /* (purple) */
+#define gfmType_reserved_6   9  /* (yellow) */
+#define gfmType_reserved_7   10 /* (light red) */
+#define gfmType_reserved_8   11 /* (beige) */
+#define gfmType_reserved_9   12 /* (blue) */
+#define gfmType_reserved_10  13 /* (dirty yellow/green) */
+#define gfmType_reserved_11  14 /* (white) */
+#define gfmType_reserved_12  15 /* No defined color (yet) */
+#define gfmType_reserved_13  16 /* No defined color (yet) */
+#define gfmType_reserved_14  17 /* No defined color (yet) */
+#define gfmType_reserved_15  18 /* No defined color (yet) */
+#define gfmType_reserved_16  19 /* No defined color (yet) */
+#define gfmType_reserved_17  20 /* No defined color (yet) */
+#define gfmType_reserved_18  21 /* No defined color (yet) */
+#define gfmType_reserved_19  22 /* No defined color (yet) */
+#define gfmType_reserved_20  23 /* No defined color (yet) */
+#define gfmType_reserved_21  24 /* No defined color (yet) */
+#define gfmType_reserved_22  25 /* No defined color (yet) */
+#define gfmType_reserved_23  26 /* No defined color (yet) */
+#define gfmType_reserved_24  27 /* No defined color (yet) */
+#define gfmType_reserved_25  28 /* No defined color (yet) */
+#define gfmType_reserved_26  29 /* No defined color (yet) */
+#define gfmType_reserved_27  30 /* No defined color (yet) */
+#define gfmType_reserved_28  31 /* No defined color (yet) */
 #define gfmType_max          32
 
 #endif /* __GFMTYPES_H__ */
