abf7179eac877927bba04a132dec44017572896a
SirGFM <gfmandaji@hotmail.com>
2016-11-28 21:06:06 -0200

    Add a second texture to ensure that the framework works with multiple textures

--- tst/gframe_debug_print_tst.c
+++ tst/gframe_debug_print_tst.c
@@ -7,19 +7,28 @@
 #include <GFraMe/gfmAssert.h>
 #include <GFraMe/gfmDebug.h>
 #include <GFraMe/gfmError.h>
+#include <GFraMe/gfmTilemap.h>
+#include <GFraMe/gfmSpriteset.h>
 
 #include <GFraMe_int/gfmCtx_struct.h>
 
 // Set the game's FPS
 #define FPS       60
+/** Create a tilemap in a lazy way */
+int pTmData[] = {
+#include "assets/map.csv"
+};
 
 int main(int arg, char *argv[]) {
     gfmCtx *pCtx;
-    int x, y;
+    gfmTilemap *pTMap;
+    gfmSpriteset *pSset;
+    int tex, x, y;
     gfmRV rv;
 
     // Initialize every variable
     pCtx = 0;
+    pTMap = 0;
 
     // Try to get a new context
     rv = gfm_getNew(&pCtx);
@@ -28,7 +37,21 @@ int main(int arg, char *argv[]) {
     ASSERT_NR(rv == GFMRV_OK);
 
     // Initialize the window
-    rv = gfm_initGameWindow(pCtx, 320, 240, 640, 480, 0, 0);
+    rv = gfm_initGameWindow(pCtx, 160, 120, 640, 480, 1, 0);
+    ASSERT_NR(rv == GFMRV_OK);
+
+    rv = gfm_loadTextureStatic(&tex, pCtx, "tm_atlas.bmp", 0xff00ff);
+    ASSERT_NR(rv == GFMRV_OK);
+    rv = gfm_createSpritesetCached(&pSset, pCtx, tex, 8/*x*/, 8/*h*/);
+    ASSERT_NR(rv == GFMRV_OK);
+
+    // Create and load the tilemap
+    rv = gfmTilemap_getNew(&pTMap);
+    ASSERT_NR(rv == GFMRV_OK);
+    rv = gfmTilemap_init(pTMap, pSset, 20/*mapWidth*/, 15/*mapHeight*/,
+            0/*defTile*/);
+    ASSERT_NR(rv == GFMRV_OK);
+    rv = gfmTilemap_loadStatic(pTMap, pTmData, 20/*mapWidth*/, 15/*mapHeight*/);
     ASSERT_NR(rv == GFMRV_OK);
 
     // Initalize the FPS counter
@@ -65,6 +88,9 @@ int main(int arg, char *argv[]) {
             rv = gfmInput_getPointerPosition(&x, &y, pCtx->pInput);
             ASSERT_NR(rv == GFMRV_OK);
 
+            rv = gfmTilemap_draw(pTMap, pCtx);
+            ASSERT_NR(rv == GFMRV_OK);
+
             gfmDebug_printf(pCtx, 4/*x*/, 4/*y*/, "MOVE YOUR MOUSE:\n%s(%03X, %03X)", pos, x, y);
             gfmDebug_printf(pCtx, x, y - 12, "POS: (%i, %i)", x, y);
 
@@ -75,6 +101,7 @@ int main(int arg, char *argv[]) {
 
     rv = GFMRV_OK;
 __ret:
+    gfmTilemap_free(&pTMap);
     gfm_free(&pCtx);
 
     return rv;
