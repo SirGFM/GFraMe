275ba2e580d51a13acbf2ef8a81fe8747d1b22f4
SirGFM <gfmandaji@hotmail.com>
2023-09-17 10:47:45 -0300

    gfmAssert: Fix compilation on EMCC

--- include/GFraMe/gfmAssert.h
+++ include/GFraMe/gfmAssert.h
@@ -14,6 +14,7 @@
 
 #if defined(__EMSCRIPTEN__) || defined(EMSCRIPTEN) || defined(EMCC)
 #  include <emscripten.h>
+#  include <stdio.h>
 #  define GOTO(rv, label) \
     do { \
       printf("[%s - %s (%d)] - Something broke here!\n", __FILE__, \
